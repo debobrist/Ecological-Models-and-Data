591b2f645322ccdc190fb68a82b521f34590b028
Debora Obrist <debora.obrist@live.ca>
2017-07-09 20:42:29 -0700

    Finished lab 3

--- Lab3_working.R
+++ Lab3_working.R
@@ -1,43 +1,42 @@
-# Part 1: Model Comparison
-BH.fit$value
-dBH.fit$value
+# Continuing on from Lab_2_Obrist.R
 
-# negative log-likelihoods are already negative and log-transformed.
+# Part 1: Model Comparison
+
+# Compare two fitted models made in lab 2. 
+# Their negative log-likelihoods are already negative and log-transformed.
+
+
+#### Q1: ####
 
 D <- (2*BH.fit$value) - (2*dBH.fit$value)
 D
 
 pchisq(D, df=1, lower.tail=FALSE)
 
-#### Q1: ####
-
-# Give the value of the test statistic and the associated p-value in 
-# your table. What does this p-value mean, in terms of probabilities? 
-# What do you conclude? Which model is 'better'?
-
 # D = 0.9858
 # p-value = 0.3207711
 
+#### Q2: ####
+
 AIC.BH <- 2*3 + 2*BH.fit$value
-AIC.BH
+AIC.BH # 507.2444
 
 AIC.dBH <- 2*4 + 2*dBH.fit$value
-AIC.dBH
+AIC.dBH # 508.2585
 
 deltaAIC <- AIC.dBH - AIC.BH
-deltaAIC
+deltaAIC # 1.01417
 
-#### Q2: ####
-# Discuss AIC stuff
 
 #### Q3: ####
 
 # Parameter estimates! 
 
-boot <- sample(c(1:nrow(sardines)), replace=T)
-boot
-
+# Make empty vector to store generated delta values.
 deltas <- NULL
+
+# Resample data (with replacement), store results of new delta values in deltas 
+# vector. 
 
 for (i in c(1:1000)){
   boot <- sample(c(1:nrow(sardines)), replace=T)
@@ -48,14 +47,23 @@ for (i in c(1:1000)){
         control=list(parscale=p.dBH, maxit=500000))
   deltas[i] <- dBH.fit$par[4]
 }
+?sample
+# Histogram of deltas: 
 
-hist(deltas, xlab = "Deltas")
+hist(deltas, 
+     xlab = "Delta", 
+     col = "grey", 
+     main = "")
 
+# Deltas are in random order - sort the deltas
 deltas <- sort(deltas)
 deltas[c(26, 975)] #CI: 0.7388837 to 3.0177816
 
+
+
 #### Q4: ####
 
+# Define parameters
 alpha <- BH.fit$par[1]
 K <- BH.fit$par[2]
 sigma <- BH.fit$par[3]
@@ -95,16 +103,35 @@ sims$BH.nLL = 0
 sims$dBH.nLL = 0
 sims$delta = -999999 # We don't want to confuse this with an estimated delta.
 
-head(sims)
+# Create loop to fit 2 new models, store -log(L) values for each model in new column
+# in sims data frame.
 
-for (i in 1:nrows(sims)){
-  recruits <- as.numeric(sims[i,1:29])
-  sims$BH.nLL[i] <- optim(par=p.BH, fn=negLL.BH, 
-                          stock=sardines$SSB, method="Nelder-Mead", 
-                          control=list(parscale=p.BH, maxit=500000))
-  sims$dBH.nLL[i]optim(par=p.dBH, fn=negLL.dBH, 
-                       stock=sardines$SSB, method="Nelder-Mead", 
-                       control=list(parscale=p.dBH, maxit=500000))
+# Also calculate the t-statistic and the p-value for both models for each set of 
+# data points.
+
+for (i in 1:nrow(sims)) {
+  BH.sfit <- optim(par=p.BH, fn=negLL.BH, 
+                   recruits= as.numeric(sims[i,1:29]),
+                   stock=sardines$SSB, method="Nelder-Mead", 
+                   control=list(parscale=p.BH, maxit=500000))
+  dBH.sfit <- optim(par=p.dBH, fn=negLL.dBH, 
+                    recruits= as.numeric(sims[i,1:29]),
+                    stock=sardines$SSB, method="Nelder-Mead", 
+                    control=list(parscale=p.dBH, maxit=500000))
+  sims$BH.nLL[i] <- BH.sfit$value
+  sims$dBH.nLL[i] <- dBH.sfit$value
+  sims$delta[i] <- dBH.sfit$par[4]
+  sims$t[i] <- (2*BH.sfit$value) - (2*dBH.sfit$value)
+  sims$p[i] <- pchisq(sims$t[i], df=1, lower.tail=FALSE)
 }
 
+#### Q5: ####
 
+length(sims$p[sims$p < 0.05]) / 1000 * 100
+# 99.9% of simulations had statistically significant p-values, which means that because
+# more complex model (the depensatory model) was significantly better than the simpler 
+# one.
+
+range(sims$delta) # All simulations had best-fit predictions of delta over 1, so the 
+# proportion of simulations in which the depensatory model was significantly better AND 
+# the best-fit predictors of delta over 1 is .999. (99.9/100)
